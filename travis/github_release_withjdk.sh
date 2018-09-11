41d66b561fbcba34f89b4ae54bd2c715acf742d9
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2018-09-11 17:12:57 +0700

    Update github_release_withjdk.sh

--- travis/github_release_withjdk.sh
+++ travis/github_release_withjdk.sh
@@ -3,7 +3,7 @@ set -e
 COMMIT=$@
 
 REPO="gama-platform/gama"
-RELEASE="latest"
+RELEASE="v1.8-rc2"
 thePATH="/home/travis/build/gama-platform/gama/ummisco.gama.product/target/products/Gama1.7"
 
 
@@ -31,28 +31,28 @@ echo $SUFFIX
 
 n=0
 RELEASEFILES[$n]="$thePATH-linux.gtk.x86.zip"
-NEWFILES[$n]='GAMA1.8_Linux_32'$SUFFIX
+NEWFILES[$n]='GAMA1.8_RC2_Linux_32'
 n=1
 RELEASEFILES[$n]="$thePATH-linux.gtk.x86_64.zip"
-NEWFILES[$n]='GAMA1.8_Linux_64'$SUFFIX
+NEWFILES[$n]='GAMA1.8_RC2_Linux_64'
 n=2
 RELEASEFILES[$n]="$thePATH-macosx.cocoa.x86_64.zip"
-NEWFILES[$n]='GAMA1.8_Mac_64'$SUFFIX
+NEWFILES[$n]='GAMA1.8_RC2_Mac_64'
 n=3
 RELEASEFILES[$n]="$thePATH-win32.win32.x86.zip"
-NEWFILES[$n]='GAMA1.8_Win_32'$SUFFIX
+NEWFILES[$n]='GAMA1.8_RC2_Win_32'
 n=4
 RELEASEFILES[$n]="$thePATH-win32.win32.x86_64.zip" 
-NEWFILES[$n]='GAMA1.8_Win_64'$SUFFIX
+NEWFILES[$n]='GAMA1.8_RC2_Win_64'
 n=5
 RELEASEFILES[$n]="$thePATH-linux.gtk.x86_64_withJDK.zip"
-NEWFILES[$n]='GAMA1.8_Linux_64_withJDK'$SUFFIX
+NEWFILES[$n]='WithJDK_GAMA1.8_Linux_64'
 n=6
 RELEASEFILES[$n]="$thePATH-win32.win32.x86_64_withJDK.zip" 
-NEWFILES[$n]='GAMA1.8_Win_64_withJDK'$SUFFIX
+NEWFILES[$n]='WithJDK_GAMA1.8_RC2_Win_64'
 n=7
 RELEASEFILES[$n]="$thePATH-macosx.cocoa.x86_64_withJDK.zip"
-NEWFILES[$n]='GAMA1.8_Mac_64_withJDK'$SUFFIX
+NEWFILES[$n]='WithJDK_GAMA1.8_RC2_Mac_64'
 
 
 git clone --depth=50 --branch=master https://github.com/gama-platform/jdk.git  jdk	
