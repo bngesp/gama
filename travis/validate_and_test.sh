f6ceeea0eb8638b1cde03be4addb93621716f905
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2019-05-17 14:40:20 +0700

    show extracted files, test travis

--- travis/validate_and_test.sh
+++ travis/validate_and_test.sh
@@ -20,9 +20,6 @@ exec java -cp $GAMA -Xms512m -Xmx$memory  -Djava.awt.headless=true org.eclipse.c
 
 res=$?
 		
-if [[ $res -gt 0 ]]; then	
-	rm -rf $passWork
-	
 	echo "after first launch"
 	cd /home/travis/build/gama-platform/gama/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/configuration/org.eclipse.osgi
 	find . | grep "\.gaml$"
@@ -30,6 +27,9 @@ if [[ $res -gt 0 ]]; then
 
 	cd /home/travis/build/gama-platform/gama/ummisco.gama.product/target/products/ummisco.gama.application.product/linux/gtk/x86_64/headless
 
+if [[ $res -gt 0 ]]; then	
+	rm -rf $passWork
+	
 
 	exit $res
 fi
